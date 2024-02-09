7b06a18fd5634f53d03fe928b13b1713cb79a72f
sebastiancald <64433491+sebastiancald@users.noreply.github.com>
2024-02-08 21:00:51 -0500

    header and footer
    Se agrega el header y footer

---
+++ clientesApp/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Bienvenido a Angular ';
+  curso: string = 'Curso spring 5 con angular 15';
+}
---
+++ clientesApp/src/app/directiva/directiva.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directiva',
+  templateUrl: './directiva.component.html',
+  styleUrls: ['./directiva.component.css']
+})
+export class DirectivaComponent {
+
+  listaCurso: string[] = ['typescript', 'java'];
+
+}
